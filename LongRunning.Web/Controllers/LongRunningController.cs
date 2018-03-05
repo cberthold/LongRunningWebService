44465eb4fc2d0f3f4c585a60ea13120dab784cb9
Chris Berthold <cberthold@baxtertechnology.com>
2018-03-04 22:56:19 -0500

    add the long running service and wire up to allow checking of status

--- LongRunning.Web/Controllers/LongRunningController.cs
+++ LongRunning.Web/Controllers/LongRunningController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using LongRunning.Web.Background;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -13,24 +14,44 @@ namespace LongRunning.Web.Controllers
     [Route("api/LongRunning")]
     public class LongRunningController : Controller
     {
-        [HttpGet("[action]")]
-        public async Task<IActionResult> Start(CancellationToken token)
+        private static readonly ConcurrentDictionary<Guid, Task<LongRunningTaskResult>> concurrent = new ConcurrentDictionary<Guid, Task<LongRunningTaskResult>>();
+        private readonly ILongRunningProcessService process;
+        public LongRunningController(ILongRunningProcessService process)
         {
-            var taskId = await StartTaskRunningAsync(token);
+            this.process = process;
+        }
+
+        [HttpGet("[action]")]
+        public IActionResult Start()
+        {
+            var taskId = StartTaskRunningAsync();
             var uri = Url.Action("Check", new { taskId });
             return Accepted(uri);
         }
 
         [HttpGet("[action]")]
-        public async Task Check(Guid taskId)
+        public async Task<IActionResult> Check(Guid taskId)
         {
+            if (concurrent.TryGetValue(taskId, out var task))
+            {
+                if (task.IsCompleted)
+                {
+                    return Ok(task.Result);
+                }
+            }
 
+            await Task.Delay(50);
+            
+            var uri = Url.Action("Check", new { taskId });
+            return Accepted(uri);
         }
 
-        private async Task<Guid> StartTaskRunningAsync(CancellationToken token)
+        private Guid StartTaskRunningAsync()
         {
             var taskId = Guid.NewGuid();
-            return await Task.FromResult(taskId);
+            var task = process.ExecuteLongRunningTask(taskId);
+            concurrent.TryAdd(taskId, task);
+            return taskId;
         }
     }
 }
