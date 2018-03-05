192b79fe349271d3b07a2ce0a88ae4b4a315b92a
Chris Berthold <cberthold@baxtertechnology.com>
2018-03-04 22:55:49 -0500

    add the long running process service which simulates a long running service

---
+++ LongRunning.Web/Background/LongRunningProcessService.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LongRunning.Web.Background
+{
+    public interface ILongRunningProcessService
+    {
+        Task<LongRunningTaskResult> ExecuteLongRunningTask(Guid taskId);
+    }
+
+    public class LongRunningProcessService : ILongRunningProcessService
+    {
+        private readonly IServiceScopeFactory serviceScopeFactory;
+        public LongRunningProcessService(IServiceScopeFactory serviceScopeFactory)
+        {
+            this.serviceScopeFactory = serviceScopeFactory;
+        }
+
+        async Task<LongRunningTaskResult> Process(Guid taskId)
+        {
+            using (var scope = serviceScopeFactory.CreateScope())
+            {
+                return await ProcessInScope(taskId, scope.ServiceProvider);
+            }
+        }
+
+        async Task<LongRunningTaskResult> ProcessInScope(Guid taskId, IServiceProvider serviceProvider)
+        {
+            await Task.Delay(15000);
+            return new LongRunningTaskResult
+            {
+                Result = $"Got {taskId.ToString("N")} at {DateTime.Now.ToShortTimeString()}",
+            };
+        }
+
+        public Task<LongRunningTaskResult> ExecuteLongRunningTask(Guid taskId)
+        {
+            TaskCompletionSource<LongRunningTaskResult> tcs1 = new TaskCompletionSource<LongRunningTaskResult>();
+            
+            // Start a background task that will complete tcs1.Task
+            Task.Factory.StartNew(async () =>
+            {
+                var result = await Process(taskId);
+                tcs1.SetResult(result);
+            });
+            return tcs1.Task;
+
+        }
+    }
+
+    public class LongRunningTaskResult
+    {
+        public string Result { get; set; }
+    }
+}
--- LongRunning.Web/Startup.cs
+++ LongRunning.Web/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LongRunning.Web.Background;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.SpaServices.Webpack;
@@ -23,6 +24,7 @@ namespace LongRunning.Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddSingleton<ILongRunningProcessService, LongRunningProcessService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
